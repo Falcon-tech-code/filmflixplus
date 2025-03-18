52cda92ffb2d43345514dff51cc91a18afdb4d94
Hasan Emre Karabacak <hsn.dev@hotmail.com>
2025-03-18 23:13:30 +0300

    refactor: restructure CSS files and implement movie favorites context project ended

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import Favorites from './pages/Favorites'
-import Home from './pages/Home'
-import { Routes, Route } from 'react-router-dom'
-import NavBar from './components/Navbar'
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
 
 function App() {
-
-
-
     return (
-        <>
-            <div>
-                <NavBar />
-                <main className='main-content'>
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/favorites' element={<Favorites />} />
-                    </Routes>
-                </main>
-            </div>
-        </>
-    )
+        <MovieProvider>
+            <NavBar />
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                </Routes>
+            </main>
+        </MovieProvider>
+    );
 }
 
-export default App
+export default App;
